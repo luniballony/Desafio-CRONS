89ef93f4a85503193ddbea2d682fee6303c5a3d3
Matilde <misabelpcarmo@gmail.com>
2025-08-25 17:53:42 +0100

    Simples implementação CRON
    - pequena implementação de CRON para testar

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,19 @@
-console.log("hi!")
+import cron from "node-cron";
+
+const seconds = "This message shows every 2 seconds"
+const timer = "*/2 * * * * *";
+
+// função para definir o cron
+function message (schedule, body) {
+    cron.schedule(schedule, () => {
+    console.log(body);
+    });
+}
+
+// função para correr o cron + mandar notif
+function program (schedule, body) {
+    message(schedule, body);
+    console.log(`CRON was acticvated at: ${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`)
+}
+
+program(timer, seconds)
