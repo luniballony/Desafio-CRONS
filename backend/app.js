f5e1215c50a0e8bfdb22ec8e6ae2596b8ff502c7
Matilde <misabelpcarmo@gmail.com>
2025-08-25 21:52:37 +0100

    Criação funções Dia e Hora
    -  criação funções dia e hora para imprimir no resultado

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 import cron from "node-cron";
 
+
 const seconds = "This message shows every 2 seconds"
 const timer = "*/2 * * * * *";
 
+/*
 // função para definir o cron
 function message (schedule, body) {
     cron.schedule(schedule, () => {    
@@ -25,3 +27,19 @@ function program (schedule, body) {
 }
 
 program(timer, seconds)
+*/
+
+// função para mostrar a hora atual
+function Time() {
+    const now = new Date();
+    return `${String(now.getHours()).padStart(2,'0')}:${String(now.getMinutes()).padStart(2,'0')}:${String(now.getSeconds()).padStart(2,'0')}`;
+}
+
+// função para mostrar dia atual
+function Day() {
+    const now = new Date();
+    return `${now.getFullYear()}-${String(now.getMonth()+1).padStart(2,'0')}-${String(now.getDate()).padStart(2,'0')}`;
+}
+
+
+console.log(`${Day()} ${Time()}`);
