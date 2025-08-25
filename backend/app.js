062ffa67a5f960e5377dc33106e7675ba9b41260
Matilde <misabelpcarmo@gmail.com>
2025-08-25 20:28:13 +0100

    CRON + tempo (pequena atualização)
    - sempre que cron corre mostra as horas a que é chamada
    - ex.:
    20:26:00 - This message shows every 2 seconds

--- backend/app.js
+++ backend/app.js
@@ -5,15 +5,23 @@ const timer = "*/2 * * * * *";
 
 // função para definir o cron
 function message (schedule, body) {
-    cron.schedule(schedule, () => {
-    console.log(body);
+    cron.schedule(schedule, () => {    
+    const now = new Date();
+
+    console.log(`${String(now.getHours()).padStart(2,'0')}:${String(now.getMinutes()).padStart(2,'0')}:${String(now.getSeconds()).padStart(2,'0')} - ${body}`  
+    );
+
     });
 }
 
 // função para correr o cron + mandar notif
 function program (schedule, body) {
     message(schedule, body);
-    console.log(`CRON was acticvated at: ${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`)
+
+    
+    const now = new Date();
+    console.log(`CRON was acticvated at: ${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
+    console.log(`CRON runs every 2 seconds.`)
 }
 
 program(timer, seconds)
