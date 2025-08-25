6968f350779df7fc8a56c4db02004bc24e8b9733
Matilde <misabelpcarmo@gmail.com>
2025-08-25 21:58:39 +0100

    Pequena Limpeza de código
    - eliminação código redundante
    - renomeação função principal e variáveis

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,9 @@
 import cron from "node-cron";
 
 
-const seconds = "This message shows every 2 seconds"
-const timer = "*/2 * * * * *";
+const temporaryMessage = "This message shows every 2 seconds"
+const frequency = "*/2 * * * * *";
 
-/*
-// função para definir o cron
-function message (schedule, body) {
-    cron.schedule(schedule, () => {    
-    const now = new Date();
-
-    console.log(`${String(now.getHours()).padStart(2,'0')}:${String(now.getMinutes()).padStart(2,'0')}:${String(now.getSeconds()).padStart(2,'0')} - ${body}`  
-    );
-
-    });
-}
-
-// função para correr o cron + mandar notif
-function program (schedule, body) {
-    message(schedule, body);
-
-    
-    const now = new Date();
-    console.log(`CRON was acticvated at: ${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
-    console.log(`CRON runs every 2 seconds.`)
-}
-
-program(timer, seconds)
-*/
 
 // função para mostrar a hora atual
 function Time() {
@@ -42,4 +18,14 @@ function Day() {
 }
 
 
-console.log(`${Day()} ${Time()}`);
+// função para definir o cron
+function CronJob (schedule, body) {
+    cron.schedule(schedule, () => {        
+        console.log(`${Day()} ${Time()} - ${body}`);
+    });
+}
+
+CronJob(frequency, temporaryMessage)
+
+
+
