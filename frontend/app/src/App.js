91600a80247bfbb01601f673ba95cbe391894a3d
Matilde <misabelpcarmo@gmail.com>
2025-08-29 13:50:57 +0100

    Implementação lista de Crons no frontend
    - frontend lista todos os crons corretamente em vez de fazer console.log

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -7,18 +7,24 @@ const App = () => {
   fetch('/list')
     .then(res => res.json()) // recebe dados do servidor e transforma-os em json para poder ser usada com js
     .then(newCron => {
-      console.log(newCron);
+      setCron(newCron);
     })
     .catch(err => console.error("Error fetching data:", err));
   }
 
   useEffect(() => { 
     getList();
-  }, []);
+  }, []); // importante estar vazio para só correr quando a página é carregada
 
   return (
     <div>
       <h1>List of Crons</h1>
+      {
+        cron.map((item) => (
+          <div key={item.uri}>
+            <p> uri: {item.uri} | http method: {item.httpMethod} | schedule: {item.schedule} | time zone: TBD | body: {item.body}</p>
+          </div>))
+      }
     </div>
   );
 }
