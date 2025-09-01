8946b7e62b36dee67a1e17a7942a0f11a8330ce7
Matilde <misabelpcarmo@gmail.com>
2025-09-01 01:01:57 +0100

    CSS - Home

--- frontend/app/src/pages/Home.jsx
+++ frontend/app/src/pages/Home.jsx
@@ -1,18 +1,23 @@
 // página inicial 
 import { Link } from "react-router-dom"
+import "../index.css";
 
 function Home () {
     return (
-        <div>
+        <div className="home-container">
             <h1>CRONS Management Service</h1>
             <p>Select what you want to do:</p>
-            <Link to='/list'>List all existing Crons</Link>
-            <Link to='/create'>Create a new Cron</Link>
-            <Link to='/list'>Edit an existing Cron</Link>
-            <Link to='/list'>Delete a Cron</Link>
 
-            <p>Thank you for your visit!</p>
-        </div>)
-}
+            <div className="home-links">
+                <Link to="/list">List all existing Crons</Link>
+                <Link to="/create">Create a new Cron</Link>
+                <Link to="/list">Edit an existing Cron</Link>
+                <Link to="/list">Delete a Cron</Link>
+            </div>
+
+      <p className="home-footer">Thank you for your visit!</p>
+    </div>
+    )
+};
 
 export default Home
