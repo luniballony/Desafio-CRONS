5f31581cf956031196040613fddd2ff54097c12b
Matilde <misabelpcarmo@gmail.com>
2025-09-01 02:55:46 +0100

    pequenas alterações

--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -4,6 +4,10 @@ import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&display=swap');
+</style>
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
